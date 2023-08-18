0f0f55bb685f3324dfa057a18e574d8bcb62df13
martinyes <mmvass18@gmail.com>
2023-08-18 15:38:39 +0200

    Integrating frontend

---
+++ src/main/java/com/github/martinyes/penguinapp/PenguinAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.martinyes.penguinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenguinAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenguinAppApplication.class, args);
+    }
+
+}
